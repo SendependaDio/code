8fe761dae3864e56aed351908d3f13d1a79237eb
Rustam <thewholeksmail@gmail.com>
2020-08-16 16:15:18 +0600

    fifth commit

--- justproject/cls.h
+++ justproject/cls.h
@@ -7,37 +7,7 @@ public:
   string grant;
 
   void read_Result(){
-      ent = 100;
-      grant = "есть";
-      cout << "Количество выбранных баллов на ЕНТ: " << ent << ";" << endl;
-      cout << "Грант: " << grant << ";" << endl;
-    }
-
-    void read_Result1(){
-      ent = 109;
-      grant = "есть";
-      cout << "Количество выбранных баллов на ЕНТ: " << ent << ";" << endl;
-      cout << "Грант: " << grant << ";" << endl;
-    }
-
-    void read_Result2(){
-      ent = 93;
-      grant = "есть";
-      cout << "Количество выбранных баллов на ЕНТ: " << ent << ";" << endl;
-      cout << "Грант: " << grant << ";" << endl;
-    }
-
-    void read_Result3(){
-      ent = 68;
-      grant = "есть";
-      cout << "Количество выбранных баллов на ЕНТ: " << ent << ";" << endl;
-      cout << "Грант: " << grant << ";" << endl;
-    }
-
-    void read_Result4(){
-      ent = 60;
-      grant = "нет, не подавал";
-      cout << "Количество выбранных баллов на ЕНТ: " << ent << ";" << endl;
+      cout << "Количество набранных баллов на ЕНТ: " << ent << ";" << endl;
       cout << "Грант: " << grant << ";" << endl;
     }
 };
--- justproject/mfile.cpp
+++ justproject/mfile.cpp
@@ -5,31 +5,39 @@
 using namespace std;
 
 int main(){
-  string name;
-  while(true){
-cout << "Введи своё имя, на русском please: ";
-  cin >> name;
+    while(true){
+      Result obj;
+      cout << "Введите пожалуйста своё имя.\n";
+      cout << ">>";
+        cin >> obj.name;
+          if(obj.name == "Ваня"){
+            obj.ent = 100;
+            obj.grant = "есть";
+            obj.read_Result();
+          }
 
-  Result obj;
-  if(name == "Ваня"){
-  obj.read_Result();
-  }
+          if(obj.name == "Рустам"){
+            obj.ent = 109;
+            obj.grant = "есть";
+            obj.read_Result();
+          }
 
-  if(name == "Рустам"){
-  obj.read_Result1();
-  }
+          if(obj.name == "Гена"){
+            obj.ent = 93;
+            obj.grant = "есть";
+            obj.read_Result();
+          }
 
-  if(name == "Гена"){
-  obj.read_Result2();
-  }
+          if(obj.name == "Данил"){
+            obj.ent = 68;
+            obj.grant = "есть";
+            obj.read_Result();
+          }
 
-  if(name == "Данил"){
-  obj.read_Result3();
-  }
-
-  if(name == "Рома"){
-  obj.read_Result4();
-  }
-  }
+          for(int i = 0; i < 40; ++i){
+            cout << "=";
+          }
+          cout << "\n";
+    }
   return 0;
 }
