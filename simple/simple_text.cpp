96447cd929ff496e28ec925a39c75de88949cf60
Rustam <thewholeksmail@gmail.com>
2020-08-21 16:15:24 +0600

    18 commit

--- simple/simple_text.cpp
+++ simple/simple_text.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <string>
 
 using namespace std;
@@ -11,5 +11,28 @@ int main(){
   cin >> z;
   if(z == 'n')
   cout << "Третье сообщение." << endl;
+  return 0;
+}*/
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+int main()
+{
+  for(int i = 0; i < 20; ++i){
+    cout << "#";
+  }
+  cout << endl;
+  cout << "Первое сообщение." << endl;
+    this_thread::sleep_for(chrono::milliseconds(3000));
+  cout << "Второе сообщение." << endl;
+    this_thread::sleep_for(chrono::milliseconds(3000));
+  cout << "Третье сообщение." << endl;
+  for(int i = 0; i < 20; ++i){
+    cout << "#";
+  }
+  cout << endl;
+
   return 0;
 }
